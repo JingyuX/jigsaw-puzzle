1c9f96e454f0a790b1df93a77c067f9a239fa2fa
JingyuX <40952657+JingyuX@users.noreply.github.com>
2019-07-08 10:53:31 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "qtcyberdip.h"
+#include <QtWidgets/QApplication>
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+	QApplication a(argc, argv);
+	qtCyberDip w;
+	w.show();
+	return a.exec();
+}
